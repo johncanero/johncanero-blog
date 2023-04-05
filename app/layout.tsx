aed4340b11ff5e3583288411203cf54c07fc70ca
John Canero <johncanero.official@gmail.com>
2023-04-05 18:13:21 +0800

    Delete app.tsx and add an app folder and create new file: page.tsx (restart server)

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: 'John Cañero Blog',
+  description: 'Blog of John Cañero',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head/>
+      <body>{children}</body>
+    </html>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 1. Add experiment appDirectory and state experimental code
+  experimental: {
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
