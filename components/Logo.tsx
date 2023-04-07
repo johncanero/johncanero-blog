cea75c4d93ca429677ced9fd2705dbad7dbd7aab
John Canero <johncanero.official@gmail.com>
2023-04-08 00:48:59 +0800

    Modify Sanity IO Components

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,19 @@
-function Logo() {
+import Image from "next/image";
+
+function Logo(props: any) {
+  const { renderDefault, title } = props;
   return (
-    <div>Logo</div>
-  )
+    <div className="flex items-center">
+      <Image
+        className="object-cover rounded-full"
+        width={50}
+        height={50}
+        src="/images/johncaneroProfile.jpg"
+        alt="Logo"
+      />
+      {renderDefault && <>{renderDefault(props)}</>}
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
--- components/StudioNavbar.tsx
+++ components/StudioNavbar.tsx
@@ -4,11 +4,19 @@ import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 function StudioNavbar(props: any) {
   return (
     <div>
-      <div className="flex items-center justify-between p-5">
+      <div className="items-center justify-between hidden p-5 md:flex font-Montserrat">
         <Link href="/" className="text-[#0ea5e9] flex items-center">
-          <ArrowUturnLeftIcon className="h-6 w-6 mr-2 text-[#0ea5e9] " />
+          <ArrowUturnLeftIcon className="h-6 w-6 mr-2 text-[#0ea5e9]" />
           Go To Website
         </Link>
+        <div className="p-5 rounded-lg justify-center border-2 border-[#0ea5e9]">
+          <h1 className="dark:text-white">Want to check out my portfolio and see my progress as a developer? 👉 </h1>
+          <Link
+            href="https://johncanero.com/"
+            className="text-[#0ea5e9] font-bold"
+          >https://johncanero.com/
+          </Link>
+        </div>
       </div>
       <>{props.renderDefault(props)}</>
     </div>
