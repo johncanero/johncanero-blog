aa35b8e6eea0301fc37d33bfd5f4fdd4d8c537bf
John Canero <johncanero.official@gmail.com>
2023-04-07 13:59:51 +0800

    Setting up Sanity Studio (Sanity.io)

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import React from 'react'
 function HomePage() {
   return (
     // 1. Created an app folder and input page.tsx
+    // 2. Install Next Sanity
+    // 3. npm (importing Sanity Project)
+    // 4. Input localhost:3000/studio
     <div>
       <h1 className='text-4xl font-semibold text-red-400 font-Montserrat'>Welcome to John Cañero's Blog</h1>
       <p className='font-light font-Montserrat'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-      {/* TimeStamp: 16:02 */}
+      {/* TimeStamp: 24:20 */}
       {/* https://www.youtube.com/watch?v=x3fCEPFgUSM&t=2915s*/}
     </div>
   )
