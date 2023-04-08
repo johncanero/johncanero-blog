0b6499d89b7709bc402960af128c70838eb41111
John Canero <johncanero.official@gmail.com>
2023-04-08 23:21:04 +0800

    Add Files to Works for Blog

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import { Montserrat } from '@next/font/google'
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
 
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '700', '800', '900'],
-  variable: '--font-montserrat'
-})
+const query = groq`
+*[_type=="post"] {
+  ...,
+  author->,
+  categories[]->,
+} | order(_createdAt desc)
+`;
+export const revalidate = 60;
 
-
-// racfe or rcfe (starter)
-function HomePage() {
-  return (
-    // 1. Created an app folder and input page.tsx
-    // 2. Install Next Sanity
-    // 3. npm (importing Sanity Project)
-    // 4. Input localhost:3000/studio
-    <div>
-      <div className={`${montserrat.variable} font-montserrat`}>
-        <h1 className='text-3xl font-medium text-sky-500'>Welcome to John Cañero's Blog</h1>
-        <p className='font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        {/* TimeStamp: 1:03:24 */}
-        {/* https://www.youtube.com/watch?v=x3fCEPFgUSM&t=2915s*/}
-      </div>
-    </div>
-  )
+export default async function HomePage() {
+  if (previewData()) {
+    return;
+    <PreviewSuspense
+      fallback={
+        <div role="status">
+          <p className="text-center text-lg animate-pulse text-[#0ea5e9]">
+            Loading Preview Data...
+          </p>
+        </div>
+      }
+    >
+      <PreviewBlogList query={query} />
+    </PreviewSuspense>;
+  }
+  const posts = await client.fetch(query);
+  return <BlogList posts={posts} />;
 }
-
-export default HomePage
